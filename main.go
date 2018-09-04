948cc613c2d7672cdaa4fcb8a1f4ff9932e85197
dongyun.fan <dongyun.fan@mljr.com>
2018-09-04 17:10:13 +0800

    code: format the output of blockchain.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main()  {
 	bc.NewBlock("send 2 Yuan to Fan")
 
 	for index, b := range bc.Blocks {
-		fmt.Println(index, b)
+		fmt.Printf("%d, %x , %x,  %s\n",index, string(b.BlockHeader.PrevHash), string(b.BlockHeader.CurHash), b.Data)
 	}
 
 }
